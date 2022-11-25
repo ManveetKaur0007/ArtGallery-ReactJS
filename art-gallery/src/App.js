f1421a93f14325e1ce5c4dce1cfff5b835c060cc
Manveet Kaur <90703104+ManveetKaur0007@users.noreply.github.com>
2022-11-24 21:02:54 -0500

    css

--- art-gallery/src/App.js
+++ art-gallery/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import Container from '@mui/material/Container'; 
+import Grid from '@mui/material/Grid';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm" style={{backgroundColor: "lightblue", fontFamily:"cursive"}}>
+    <Grid item xs={12} style={{textAlign:"center"}}>
+        <h1>The Art Gallery</h1>
+    </Grid>
+    <Grid item xs={12}>
+      <nav>navigation area</nav>
+    </Grid>
+    <Grid item xs={12}>
+        <p>Art is often considered the process or product of deliberately arranging elements in a way that appeals 
+        to the senses or emotions. It encompasses a diverse range of human activities, creations and ways of expression, 
+        including music, literature, film, sculpture and paintings</p>
+    </Grid><br/><br/>
+    <Grid item xs={12}>
+        Project Made By:<br/>
+            <a href="https://manveetkaur0007.wixsite.com/index">Manveet Kaur</a><br/>
+            <a href="http://file:///F:/DevFolio/index.html">Simranjit Kaur</a>
+        </Grid>
+    </Container>
   );
 }
 
