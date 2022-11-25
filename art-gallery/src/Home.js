ef5df8a925afd4c7d12e79b49ee469c290cacc0c
Manveet Kaur <90703104+ManveetKaur0007@users.noreply.github.com>
2022-11-24 20:28:48 -0500

    added images

---
+++ art-gallery/src/Home.js
@@ -0,0 +1,19 @@
+//import React, {useState} from "react";
+import Container from '@mui/material/Container'; 
+import Grid from '@mui/material/Grid';
+//import Box from '@mui/material/Box';
+
+
+export default function Home() {
+  return (
+<Container maxWidth="medium">
+    <Grid item xs={12}>
+        <h1>The Art Gallery</h1>
+    </Grid>
+    <Grid item xs={12}>
+        Project Made By:
+            <a href="https://manveetkaur0007.wixsite.com/index">Manveet Kaur</a>
+            <a href="http://file:///F:/DevFolio/index.html">Simranjit Kaur</a>
+        </Grid>
+    </Container>
+)};
