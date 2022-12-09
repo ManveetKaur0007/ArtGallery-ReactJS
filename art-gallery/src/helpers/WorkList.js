4726d24c12714677856344dad3adbc547f3629ac
Manveet Kaur <90703104+ManveetKaur0007@users.noreply.github.com>
2022-12-08 22:15:14 -0500

    work list

---
+++ art-gallery/src/helpers/WorkList.js
@@ -0,0 +1,53 @@
+import Candels from "../assets/images/candels.jpg";
+import Deer from "../assets/images/deer.jpg";
+import Lion from "../assets/images/lion.jpg";
+import Moon from "../assets/images/moon.jpg";
+import Mountains from "../assets/images/mountains.jpg";
+import SideView from "../assets/images/side-view.jpg";
+import Skull from "../assets/images/skull.jpg";
+import Sunset from "../assets/images/sunset.jpg";
+import Swan from "../assets/images/swan.jpg";
+import Swing from "../assets/images/swing.jpg";
+
+export const WorkList = [
+  {
+    name: "The Smoking Candels",
+    image: Candels,
+  },
+  {
+    name: "Deer",
+    image: Deer,
+  },
+  {
+    name: "The Love Between Two",
+    image: Lion,
+  },
+  {
+    name: "Moon",
+    image: Moon,
+  },
+  {
+    name: "Mountain View",
+    image: Mountains,
+  },
+  {
+    name: "Side View of the Girl",
+    image: SideView,
+  },
+  {
+    name: "Skull with roeses",
+    image: Skull,
+  },
+  {
+    name: "Sunset View",
+    image: Sunset,
+  },
+  {
+    name: "Swan in the Lake",
+    image: Swan,
+  },
+  {
+    name: "Girl Swinging",
+    image: Swing,
+  },
+];
--- art-gallery/src/pages/Work.js
+++ art-gallery/src/pages/Work.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import { WorkList } from "../helpers/WorkList";
+import MenuItem from "../components/WorkItem";
+import "../styles/Work.css";
 
 function Work() {
   return (
-    <div>
-      <h1>Work</h1>
+    <div className="work">
+      <h1 className="workTitle">Our Work</h1>
+      <div className="workList">
+        {WorkList.map((menuItem, key) => {
+          return (
+            <MenuItem
+              key={key}
+              image={menuItem.image}
+              name={menuItem.name}
+            />
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Work
+export default Work;
